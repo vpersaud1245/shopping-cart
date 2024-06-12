89b97675c92ceb1735a15248f6966322610347c8
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-06-12 18:50:09 -0400

    feat: Add error page

---
+++ src/Components/Error/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import styles from "./ErrorPage.module.css";
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div className={styles.errorPage}>
+      <h1 className={styles.errorTitle}>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
