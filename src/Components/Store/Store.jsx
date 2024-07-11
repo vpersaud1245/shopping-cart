2a569b3c6eb622afe4235ddfb96385679f7fe4f6
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-07-11 17:27:06 -0400

    feat: Add cart functionality from store page

--- src/Components/Store/Store.jsx
+++ src/Components/Store/Store.jsx
@@ -1,11 +1,16 @@
 import { useLoaderData, Link } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar.jsx";
 import styles from "./Store.module.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { capitalizeFirstLetter, formatPrice } from "../../Util/formatters.js";
+import cartIcon from "../../assets/cartIcon.svg";
+import inCartIcon from "../../assets/inCartIcon.svg";
+import { CartContext } from "../../Cart/cart.jsx";
+import Product from "../../Product/product.js";
 
 function Store() {
+  const { addProduct, removeProduct, hasProduct } = useContext(CartContext);
   const [products, setProducts] = useState(useLoaderData());
 
   return (
@@ -34,16 +39,34 @@ function Store() {
                       alt={`Image of ${product.title}`}
                       className={styles.productImg}
                     ></img>
-                    <div className={styles.productInfo}>
-                      <p className={styles.productTitle}>
-                        {product.title.substring(0, 36)}
-                      </p>
-                      <p className={styles.productCategory}>
-                        {capitalizeFirstLetter(product.category)}
-                      </p>
-                      <p className={styles.productPrice}>
-                        {formatPrice(product.price)}
-                      </p>
+                    <div className={styles.cardLowerContainer}>
+                      {" "}
+                      <div className={styles.productInfo}>
+                        <p className={styles.productTitle}>
+                          {product.title.substring(0, 36)}
+                        </p>
+                        <p className={styles.productCategory}>
+                          {capitalizeFirstLetter(product.category)}
+                        </p>
+                        <p className={styles.productPrice}>
+                          {formatPrice(product.price)}
+                        </p>
+                      </div>
+                      <img
+                        className={styles.cartIcon}
+                        src={hasProduct(product.id) ? inCartIcon : cartIcon}
+                        alt="Cart Icon"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          if (!e.target.src.includes("inCartIcon")) {
+                            addProduct(new Product(product.id, 1));
+                            e.target.src = inCartIcon;
+                          } else if (e.target.src.includes("inCartIcon")) {
+                            removeProduct(product.id);
+                            e.target.src = cartIcon;
+                          }
+                        }}
+                      />
                     </div>
                   </motion.div>
                 </Link>
